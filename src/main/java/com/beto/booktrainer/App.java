f1a4178e63b38246d48dd595b0d715828dd9faf2
beto <beto@beto.com>
2018-12-06 18:27:24 -0500

    basic project structure

---
+++ src/main/java/com/beto/booktrainer/App.java
@@ -0,0 +1,13 @@
+package com.beto.booktrainer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
